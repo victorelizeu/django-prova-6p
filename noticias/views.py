c55e3e0da9d23f7e00a814b2fb22d36003d43504
victorelizeu <joaogamer759@gmail.com>
2026-09-23 21:09:39 -0300

    feat: Feature 1 criada.

--- noticias/views.py
+++ noticias/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render, redirect
-from .models import Post
+from .models import Post, Categoria
 from .forms import PostForm
+from django.db.models import Q
 
 # Create your views here.
 
 
 def lista_posts(request):
-    posts = Post.objects.all()
-    return render(request, "noticias/lista.html", {"posts": posts})
+
+    filtros = Q()
+
+    q = request.GET.get("q", "").strip()
+    categoria_id = request.GET.get("categoria", "").strip()
+
+    if q:
+        filtros &= Q(titulo__icontains=q)
+
+    if categoria_id:
+        filtros &= Q(categoria_id=categoria_id)
+
+    posts = Post.objects.filter(filtros)
+
+    categorias = Categoria.objects.all()
+
+    conjunto = {
+        "posts": posts,
+        "categorias": categorias,
+    }
+
+    return render(request, "noticias/lista.html", conjunto)
 
 
 def novo_post(request):
